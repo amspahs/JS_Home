aed03c852d422e18582ee39fe7597598e374c1f5
김재섭 <@192.168.21.18>
2022-07-14 07:08:32 +0900

    123

---
+++ VariableTest/src/VarMateInfoTest.java
@@ -0,0 +1,24 @@
+
+public class VarMateInfoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "안병규";
+		int age = 30;
+		System.out.println(name);
+		System.out.println(age);		
+			
+		name = "오건황";
+		System.out.println(name);
+		System.out.println(age);	
+		
+		name = "이동현";
+		age = age - 3;
+		System.out.println(name);
+		System.out.println(age);	
+				
+		
+	}
+
+}
